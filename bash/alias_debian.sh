f2379223c1f86284668c50f8f5f5de9f2ca39a48
GusBricker <chris@lapa.com.au>
2015-11-02 22:23:38 +1100

    aliases: debian: adds a few network helper aliases/functions.
    Signed-off-by: GusBricker <chris@lapa.com.au>

--- bash/alias_debian.sh
+++ bash/alias_debian.sh
@@ -5,3 +5,30 @@ alias update='sudo apt-get update && sudo apt-get upgrade'
 alias upgrade='sudo apt-get dist-upgrade'
 alias uninstall='sudo apt-get purge'
 alias remove='sudo apt-get autoremove'
+alias ifrelease='sudo dhclient -r -v'
+alias ifrenew='sudo dhclient -v'
+
+function ics_enable()
+{
+    local from_if="${1}"
+    local to_if="${2}"
+    sudo sysctl -w net.ipv4.ip_forward=1
+
+    sudo iptables -F
+    sudo iptables -t nat -F
+    sudo iptables -t mangle -F
+    sudo iptables --table nat --append POSTROUTING --out-interface "${from_if}" -j MASQUERADE
+    sudo iptables --append FORWARD --in-interface "${to_if}" -j ACCEPT
+    sudo dnsmasq --interface="${to_if}" --no-dhcp-interface="${to_if}"
+}
+
+function ics_disable()
+{
+    local from_if="${1}"
+    local to_if="${2}"
+    sudo sysctl -w net.ipv4.ip_forward=0
+
+    sudo iptables --table nat --delete POSTROUTING --out-interface "${from_if}" -j MASQUERADE
+    sudo iptables --delete FORWARD --in-interface "${to_if}" -j ACCEPT
+    sudo dnsmasq --except-interface="${to_if}" --no-dhcp-interface="${to_if}"
+}
